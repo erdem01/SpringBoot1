e81f56405a760e2b596448fac66ccf058a3279f2
superdem <superdem@kompeder>
2016-10-06 00:23:35 +0300

    multipart destegi ile dosya upload edilebilir olmasi saglanmistir

--- src/main/java/erdemc/deneme/controller/LoginController.java
+++ src/main/java/erdemc/deneme/controller/LoginController.java
@@ -1,9 +1,14 @@
 package erdemc.deneme.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.Part;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
 
 import erdemc.deneme.service.OrderService;
 import erdemc.deneme.service.UsersService;
@@ -22,6 +27,18 @@ public class LoginController {
 		System.out.println(usersService.findAll());
 		System.out.println(orderService.findAll());
 		return "login";
+	}
+	
+	@RequestMapping(value="/upload", method = RequestMethod.POST)
+	public String displayLogin(@RequestPart("profilePicture") Part mFile) {
+		try {
+			mFile.write("/home/superdem/Downloads/a.jpg");
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "redirect:/";
 	}
 	
 }
