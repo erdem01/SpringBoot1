127f438a71781114f01302020b2ccd41cb10f015
superdem <superdem@kompeder>
2016-10-09 12:47:35 +0300

    angularjs buton ornegi denendi

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,4 +1,13 @@
 var app = angular.module('app', ['ngRoute','ngResource']);
 (function() {
 	var app = angular.module('app', []);
+	app.controller('AppCtrl', [function() {
+		var helloMsg = 'Hello';
+		var goodbyeMsg = 'Hello';
+		var self = this;
+		self.message= 'Hello';
+		self.changeMessage = function() {
+			self.message = self.message === helloMsg ? goodbyeMsg : helloMsg;
+		};
+	}]);
 })();
